6b98ce199a04ebf3fc9e913641bb15a862c435d8
Michael Gutteridge <michael.gutteridge@gmail.com>
2012-08-29 10:39:27 -0700

    initial layup of package

---
+++ README.txt
@@ -0,0 +1,20 @@
+============================
+Python-Moab Workload Manager
+============================
+
+Provides a python interface into MWM objects
+
+Section 1
+=========
+
+foo
+
+Section 1.1
+-----------
+
+ 1. foo
+
+ 2. bar
+
+ 3. baz
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+        name='pyMoab',
+        version='0.0',
+        author='Michael Gutteridge',
+        author_email='mrg@fhcrc.org',
+        packages=['mwm', ],
+        url='',
+        license='LICENSE.txt',
+        description='Modules for interfacing with MWM.',
+        long_description=open('README.txt').read(),
+        install_requires=[
+                ],
+)
