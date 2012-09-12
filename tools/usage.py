86c7f9918965b49969c084e5aa322365e96e5625
Michael Gutteridge <michael.gutteridge@gmail.com>
2012-09-12 10:49:53 -0700

    first fully functional version

--- tools/usage.py
+++ tools/usage.py
@@ -7,6 +7,62 @@ import xml.etree.ElementTree as et
 s = Scheduler()
 summary = {}
 
+# Joe User using 12 cores on one private node, 24 on 4 public nodes
+# Account for Joe User (user_j) with Bob Barker using 24 cores on 4 public nodes and Alice Alignment using 12 cores on one private node
+# 
+# each account has records like:
+# AS {
+# 'name': 'user_j'
+# {
+#   'aa': { 'private':[ 1, 12 ], 'public':[ 0, 0 ] },
+#   'bb': { 'private':[ 0, 0 ], 'public':[ 4, 24 ] }
+# }
+#}
+
+# account_summary.getNodeTotal() = [ n, m ] where
+# n is the sum of private nodes in use for all users and m
+# the sum of all public nodes
+
+class AS():
+    def __init__(self, name):
+        self.name = name
+        self.users = {}
+        pass
+    
+    def _create_user( self, user ):
+        if user not in self.users:
+            self.users[ user ] = {'private':[0,0], 'public':[0,0] }
+        else:
+            pass
+
+    def addJob(self, user, pvnodes=0, pvcores=0, pbnodes=0, pbcores=0 ):
+        self._create_user( user )
+        self.users[ user ][ 'private' ][0] += pvnodes
+        self.users[ user ][ 'private' ][1] += pvcores
+        self.users[ user ][ 'public' ][0] += pbnodes
+        self.users[ user ][ 'public' ][1] += pbcores
+
+    def summary( self ):
+        pvn = 0
+        pvc = 0
+        pbn = 0
+        pbc = 0
+        for user in self.users:
+            pvn = pvn + self.users[ user ]['private'][0]
+            pvc = pvc + self.users[ user ]['private'][1]
+            pbn = pbn + self.users[ user ]['public'][0]
+            pbc = pbc + self.users[ user ]['public'][1]
+        return [ pvn, pvc, pbn, pbc ]
+
+    def userSummary( self, user ):
+        return[
+            self.users[ user ]['private'][0],
+            self.users[ user ]['private'][1],
+            self.users[ user ]['public'][0],
+            self.users[ user ]['public'][1],
+        ]
+
+
 r = et.fromstring(
     s.doCommand( [ "mdiag", "--xml", '-r', ] )
 )
@@ -22,30 +78,71 @@ r = et.fromstring(
     s.doCommand( [ "mdiag", "--xml", '-j', ] )
 )
 
+# total = {'acctname':AS()}}
+total = {}
+
 for q in r.findall( 'job' ):
     job_details= q.attrib
     job_details.update( q.find( 'req' ).attrib )
     j = Job( job_details )
     if j.State == 'Running':
-        print '-' * 80
         prv_core_count = 0
         oth_core_count = 0
-        prv_nodes = set(j.AllocNodeList.keys()).intersection( set(reserved_nodes.keys()) )
-        other_nodes = set(j.AllocNodeList.keys()).difference( set(reserved_nodes.keys()) )
 
-        print "JobID: {}".format( j.JobID, )
-        print "Account: {}".format( j.Account, )
-        print "User: {}".format( j.User, )
+        prv_nodes = set(j.AllocNodeList.keys()).intersection(
+            set(reserved_nodes.keys()) )
+        other_nodes = set(j.AllocNodeList.keys()).difference(
+            set(reserved_nodes.keys()) )
+
+
+        if j.Account not in total:
+            total[ j.Account ] = AS( j.Account )
+
         for m in prv_nodes:
             if reserved_nodes[ m ] == 0 :
                 prv_core_count = prv_core_count + 12
             else:
                 prv_core_count = prv_core_count + reserved_nodes[ m ]
-        print "Private Cores/Nodes: {}/{}".format( prv_core_count, prv_nodes )
         for m in other_nodes:
             if j.AllocNodeList[ m ] == 0 :
                 oth_core_count = oth_core_count + 12
             else:
                 oth_core_count = oth_core_count + j.AllocNodeList[ m ]
-        print "Other Cores/Nodes: {}/{}".format( oth_core_count, other_nodes )
+
+        total[ j.Account ].addJob(
+            j.User,
+            len( prv_nodes ),
+            prv_core_count,
+            len( other_nodes ),
+            oth_core_count,
+        )
+
+ofmt = "{:>25}{:^15}{:^15}"
+print ofmt.format( "user", "private", "public" )
+ofmt = "{:>25}{:>6} / {:<4}{:>6} / {:<4}"
+print ofmt.format( "account total","nodes", "cores","nodes", "cores" )
+print "-" * 53
+
+pvn_total = 0
+pvc_total = 0
+pbn_total = 0
+pbc_total = 0
+for acct, s in total.items():
+    ofmt = "{:>25}{:>6} / {:<5}{:>6} / {:<5}"
+    for u in s.users:
+        t = s.userSummary(u)
+        print ofmt.format(
+            u, *(s.userSummary(u)) )
+
+    ofmt = "{:>25}{:>6} / {:<5}{:>6} / {:<5}"
+    print ofmt.format( acct + " total", *(s.summary()) )
+    print "-" * 53
+
+    pvn_total = pvn_total + s.summary()[0]
+    pvc_total = pvc_total + s.summary()[1]
+    pbn_total = pbn_total + s.summary()[2]
+    pbc_total = pbc_total + s.summary()[3]
+
+print ofmt.format( "total used", pvn_total, pvc_total, pbn_total, pbc_total )
+
 
