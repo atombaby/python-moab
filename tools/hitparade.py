0f3fafb9ac10b59614dac699af18c3c17e2a4c0d
Michael Gutteridge <michael.gutteridge@gmail.com>
2012-11-07 08:29:27 -0800

    First working version
    Accurately reports on utilization of public (unreserved) resources and
    outputs a list sorted by cores used.

--- tools/hitparade.py
+++ tools/hitparade.py
@@ -1,52 +1,84 @@
 #!/usr/bin/env python
-# Show current cluster utilization grouped by user
-# User, Cores
-# pbradley 954
-# rfu 150
-# ylin2 110
-# sjiao 101
-# cqu 100
-# ckang2 16
-# syin2 2
-# krcurtis 1
-# Free 62
-# Total 1496
+"""
+Show used and available public cores. Break down used
+cores by user and account
+"""
+import sys
+sys.path.append('/opt/moab/lib/python2.7/site-packages')
+from mwm.scheduler import Scheduler
+from mwm.mtypes import Reservation, Job, Node
+import xml.etree.cElementTree as et
 
-
-from scheduler import Scheduler
-from mtypes import Job
-import xml.etree.ElementTree as et
-import operator
+bold='\033[1;47m'
+normal='\033[1;m'
 
 s = Scheduler()
-summary = {}
+
+
+node_reservations = {}
+r = et.fromstring(
+    s.doCommand( [ "mdiag", "--xml", '-r', ] )
+)
+for q in r.findall( 'rsv' ):
+    rsv = Reservation( q.attrib )
+    if rsv.Type == "User" and rsv.SubType == "StandingReservation":
+        for n in rsv.AllocNodeList:
+            node_reservations[ n ] = rsv.RsvParent
 
 r = et.fromstring(
-    s.doCommand( [ "showq", "--xml", '-r', ] )
+    s.doCommand( [ "mdiag", "--xml", '-j', ] )
 )
 
-# Used/free stats are in the "cluster" branch
+jobs = {}
+for j in r.findall( 'job' ):
+    job = Job( dict( j.attrib.items() + j.find('req').items() ))
+    if job.State == 'Running' and "PREEMPTEE" not in job.Flags:
+        jobs[ job.JobID ] = [
+            job.AllocNodeList,
+            job.User,
+            job.Account,
+        ]
 
-free_nodes = r.find( 'cluster' ).attrib[ 'LocalIdleNodes' ]
-up_nodes = r.find( 'cluster' ).attrib[ 'LocalUpNodes' ]
-free_cores = r.find( 'cluster' ).attrib[ 'LocalIdleProcs' ]
-up_cores = r.find( 'cluster' ).attrib[ 'LocalUpProcs' ]
+# create count of public nodes in campus class
+r = et.fromstring(
+    s.doCommand( [ "mdiag", "--xml", '-n', ] )
+)
+public_nodes = []
+public_cores = 0
+for n in r.findall( 'node' ):
+    node = Node( n.attrib )
+    if 'campus' in node.CFGCLASS and node.NODEID not in node_reservations:
+        public_nodes.append( node.NODEID )
+        public_cores += node.RCPROC
 
-for j in r.findall( 'queue/job' ):
-    job = Job( j.attrib )
-    try:
-        summary[ job.User ] = summary[ job.User ] + job.ReqProcs
-    except KeyError:
-        summary[ job.User ] = job.ReqProcs
+cred_totals = {}
+# { 'cred':cores }
+for job in jobs.values():
+    for node in job[0]:
+        job_cred = job[1] + " (" + job[2] + ")"
+        if node in public_nodes:
+            o = [
+                job[0][node],
+                node,
+                'public',
+                job_cred,
+            ]
+            try:
+                cred_totals[ job_cred ] = (
+                    cred_totals[ job_cred ] + job[0][node]
+                )
+            except KeyError:
+                cred_totals[ job_cred ] = job[0][node]
 
+print "{:>25} {:<5}".format( 'user (account)', 'cores' )
+print "{:->25} {:-<5}".format( '', '' )
 
-ofmt="{:>15} {}"
-print ofmt.format( 'user', 'cores' )
-for k,v in sorted( summary.iteritems(),
-        key=operator.itemgetter(1),
-        reverse=True):
-    print ofmt.format( k, v )
+sum = 0
+for k,v in sorted( cred_totals.items(), key=lambda x: x[1], reverse=True):
+    print "{:>25} {:<5}".format( k,v )
+    sum += v
 
-ofmt="{:>15} {} of {}"
-print ofmt.format( 'free nodes', free_nodes, up_nodes )
-print ofmt.format( 'free cores', free_cores, up_cores )
+print "{:->25} {:-<5}".format( '', '' )
+print "{:>25} {:<5}".format( 'total cores in use', sum )
+print "{:>25} {:<5}".format( 'total cores configured', public_cores )
+
